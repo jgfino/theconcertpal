1d1df3ef5d07bf3622d3ac08cba43de9afed23d2
Julia Finocchiaro <jgfino.dev@gmail.com>
2025-01-16 23:23:34 -0500

    tsignore

--- packages/next/src/components/Input.tsx
+++ packages/next/src/components/Input.tsx
@@ -22,6 +22,7 @@ export default function TextInput({
     <div className={`flex flex-col gap-2 ${className || ""}`}>
       {label && <label htmlFor={id}>{label}</label>}
       {multiline ? (
+        // @ts-expect-error types not implemented
         <textarea
           {...props}
           id={id}
